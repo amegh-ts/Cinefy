c38a2e974de53798ffa852e158245cb7962f9ee6
Amegh <80452444+amegh-ts@users.noreply.github.com>
2024-02-23 02:12:26 +0530

    23-02-2024

--- client/components/MovieSwiper.tsx
+++ client/components/MovieSwiper.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import './MovieSwiper.css'
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+
+import { Autoplay, EffectCoverflow } from 'swiper/modules';
 
 const MovieSwiper = () => {
-  return (
-    <div>MovieSwiper</div>
-  )
+    return (
+        <Swiper effect={'coverflow'} grabCursor={true} slidesPerView={'auto'} autoplay={{ delay: 2500, disableOnInteraction: false, }} coverflowEffect={{ rotate: 50, stretch: 0, depth: 100, modifier: 1, slideShadows: true }} loop={true} modules={[Autoplay, EffectCoverflow]} className='movieSwiper'>
+
+        </Swiper>
+    )
 }
 
 export default MovieSwiper
