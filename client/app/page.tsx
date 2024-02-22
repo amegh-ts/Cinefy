78ecb77a1a7163b62c12b5ccffaae92f8e23404a
Amegh <80452444+amegh-ts@users.noreply.github.com>
2024-02-22 15:39:08 +0530

    22-02-2024

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,9 +1,11 @@
+import Hero from "@/pages/Hero/Hero";
 import Navbar from "@/pages/navbar/Navbar";
 
 export default function Home() {
   return (
     <main>
       <Navbar />
+      <Hero />
     </main>
   );
 }
