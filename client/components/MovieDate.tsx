a9c0e4856a687252ecf77999f08ac57b575be815
Amegh <80452444+amegh-ts@users.noreply.github.com>
2024-02-23 03:52:44 +0530

    Update MovieDate.tsx

--- client/components/MovieDate.tsx
+++ client/components/MovieDate.tsx
@@ -1,6 +1,14 @@
 import React from 'react'
 
-const MovieDate = ({movie}) => {
+interface Movie {
+    date: string;
+    active?: boolean; // optional
+  }
+  
+  interface MovieDateProps {
+    movie: Movie;
+  }
+const MovieDate: React.FC<MovieDateProps> = ({movie}) => {
     return (
         <div className={`date ${movie.active ? 'active' : undefined}`}>
             <h2>On {movie.date}</h2>
