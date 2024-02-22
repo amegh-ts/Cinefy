ed77825a028c530d3f9e2dbba3601f5fc4e08d5b
Amegh <80452444+amegh-ts@users.noreply.github.com>
2024-02-23 04:20:31 +0530

    23-02-2024

--- client/components/MovieSwiper.tsx
+++ client/components/MovieSwiper.tsx
@@ -1,37 +1,89 @@
-import React from 'react'
-import './MovieSwiper.scss'
+// import React from 'react'
+// import './MovieSwiper.scss'
 
+// import { Swiper, SwiperSlide } from 'swiper/react';
+// import 'swiper/css/effect-coverflow';
+// import 'swiper/css/pagination';
+
+// import { Autoplay, EffectCoverflow } from 'swiper/modules';
+// import SwiperCore from 'swiper';
+
+// SwiperCore.use([Autoplay, EffectCoverflow]);
+
+// interface Slide {
+//     _id: string;
+//     previewImg: string;
+// }
+
+// interface MovieSwiperProps {
+//     slides: Slide[];
+//     slideChange: (id: string) => void;
+// }
+
+// const MovieSwiper: React.FC<MovieSwiperProps> = ({ slides, slideChange }) => {
+//     return (
+//         <Swiper effect={'coverflow'} grabCursor={true} slidesPerView={'auto'} autoplay={{ delay: 2500, disableOnInteraction: false, }} coverflowEffect={{ rotate: 50, stretch: 0, depth: 100, modifier: 1, slideShadows: true }} loop={true} modules={[Autoplay, EffectCoverflow]} className='movieSwiper'>
+//             {
+//                 slides.map(slides => (
+//                     <SwiperSlide key={slides._id}>
+//                         <img src={slides.previewImg} alt="Preview IMage" onClick={() => slideChange(slides._id)} />
+//                     </SwiperSlide>
+//                 ))
+//             }
+//         </Swiper>
+//     )
+// }
+
+// export default MovieSwiper
+
+import React from 'react';
+import './MovieSwiper.scss';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css/effect-coverflow';
 import 'swiper/css/pagination';
-
 import { Autoplay, EffectCoverflow } from 'swiper/modules';
 import SwiperCore from 'swiper';
 
 SwiperCore.use([Autoplay, EffectCoverflow]);
 
-interface Slide {
+interface Movie {
     _id: string;
-    previewImg: string;
+    bgImg: string;
+    active: boolean;
+    titleImg: string;
+    year: string;
+    ageLimit: string;
+    length: string;
+    category: string;
+    description: string;
+    date: string;
+    video: string;
+    title: string;
 }
 
 interface MovieSwiperProps {
-    slides: Slide[];
+    slides: Movie[]; // Change type to Movie[]
     slideChange: (id: string) => void;
 }
 
 const MovieSwiper: React.FC<MovieSwiperProps> = ({ slides, slideChange }) => {
     return (
-        <Swiper effect={'coverflow'} grabCursor={true} slidesPerView={'auto'} autoplay={{ delay: 2500, disableOnInteraction: false, }} coverflowEffect={{ rotate: 50, stretch: 0, depth: 100, modifier: 1, slideShadows: true }} loop={true} modules={[Autoplay, EffectCoverflow]} className='movieSwiper'>
-            {
-                slides.map(slides => (
-                    <SwiperSlide key={slides._id}>
-                        <img src={slides.previewImg} alt="Preview IMage" onClick={() => slideChange(slides._id)} />
-                    </SwiperSlide>
-                ))
-            }
+        <Swiper
+            effect={'coverflow'}
+            grabCursor={true}
+            slidesPerView={'auto'}
+            autoplay={{ delay: 2500, disableOnInteraction: false }}
+            coverflowEffect={{ rotate: 50, stretch: 0, depth: 100, modifier: 1, slideShadows: true }}
+            loop={true}
+            className='movieSwiper'
+        >
+            {slides.map(movie => (
+                <SwiperSlide key={movie._id}>
+                    <img src={movie.bgImg} alt="Preview Image" onClick={() => slideChange(movie._id)} />
+                </SwiperSlide>
+            ))}
         </Swiper>
-    )
+    );
 }
 
-export default MovieSwiper
+export default MovieSwiper;
