21f4960c33cda1392fa8697a42e371c38ca26a08
Amegh <80452444+amegh-ts@users.noreply.github.com>
2024-02-22 20:54:29 +0530

    Update Hero.tsx

--- client/pages/Hero/Hero.tsx
+++ client/pages/Hero/Hero.tsx
@@ -5,6 +5,7 @@ import './Hero.scss'
 import axios from 'axios';
 import { IoPlayOutline } from "react-icons/io5";
 import MovieContent from '@/components/MovieContent';
+import MovieDate from '@/components/MovieDate';
 
 
 const Hero = () => {
@@ -33,13 +34,13 @@ const Hero = () => {
                 <div className="container-fluid">
                     <div className="row">
                         <div className="col-lg-6 col-mid-12">
-                            <MovieContent/>
+                            <MovieContent />
                         </div>
                         <div className="col-lg-6 col-mid-12">
-                            
+                            <MovieDate />
 
                             <div className="trailer d-flex align-items-center justify-content-center active">
-                                <a href="#" className="playBtn"><IoPlayOutline className='play-icon'/></a>
+                                <a href="#" className="playBtn"><IoPlayOutline className='play-icon' /></a>
                                 <p>Watch Trailer</p>
                             </div>
                         </div>
