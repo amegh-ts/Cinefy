758d40a6cfb4604d972a687d72251a52e4334eb6
Amegh <80452444+amegh-ts@users.noreply.github.com>
2024-02-23 03:51:16 +0530

    Update Modal.tsx

--- client/components/Modal.tsx
+++ client/components/Modal.tsx
@@ -2,13 +2,22 @@ import React from 'react'
 import './Modal.scss'
 import { IoCloseCircleOutline } from "react-icons/io5";
 
-const Modal = ({ movie, toggleModal, status }) => {
+interface ModalProps {
+    movie: {
+      video: string;
+      title: string;
+    };
+    toggleModal: () => void;
+    status: boolean;
+  }
+
+const Modal: React.FC<ModalProps> = ({ movie, toggleModal, status }) => {
     return (
         <div className={`movieModal ${status ? 'active' : undefined}`}>
             <a href="#" className='modalClose' onClick={toggleModal}>
                 <IoCloseCircleOutline />
             </a>
-            <iframe src={movie.video} frameborder="0" width={1280} height={720} title={`${movie.title} | Official Trailer`} allow='accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' allowFullScreen></iframe>
+            <iframe src={movie.video} width={1280} height={720} title={`${movie.title} | Official Trailer`} allow='accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' allowFullScreen></iframe>
         </div>
     )
 }
