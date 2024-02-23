ba7c4c4cb4a3fc210c403cc0208f20e9bd88e0ac
Amegh <80452444+amegh-ts@users.noreply.github.com>
2024-02-23 18:36:02 +0530

    Update index.js

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const dotenv = require('dotenv')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const PORT=5000
+
+app.use(cors())
+dotenv.config()
+
+
+
+mongoose.connect(process.env.Mongo_Key).then(() => {
+    console.log('Database Connected');
+})
+
+app.use(express.json())
+
+
+app.listen(PORT, () => {
+    console.log('Connected to Server on Port',PORT);
+})
