bccdfcd372ca03bcbea11e0b15aa41007bc540c2
Amegh <80452444+amegh-ts@users.noreply.github.com>
2024-02-23 18:51:48 +0530

    Update MovieControler.js

--- server/Controller/MovieControler.js
+++ server/Controller/MovieControler.js
@@ -1,1 +1,12 @@
-const movieSchema=require()
+const movieSchema = require('../Models/MovieSchema')
+
+const addMovies = async (req, res) => {
+    try {
+        console.log("req.body", req.body);
+    } catch (error) {
+        console.error('Error adding movie:', error);
+        res.status(500).json(error);
+    }
+}
+
+module.exports = { addMovies };
